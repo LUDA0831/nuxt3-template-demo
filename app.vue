912cc646407eebc882d1afbe88398b1620424c65
SpiderMan <445001382@qq.com>
2022-12-07 14:26:41 +0800

    feat: 添加全局meta

--- app.vue
+++ app.vue
@@ -9,4 +9,11 @@
 import '@arco-design/web-vue/es/message/style/css.js'
 import '@arco-design/web-vue/es/notification/style/css.js'
 const { key } = useFixApp()
+
+useHead({
+  meta: [
+    { name: 'description', content: '我的神奇网站.我的神奇网站.我的神奇网站.我的神奇网站.我的神奇网站.我的神奇网站.' },
+    { name: 'keywords', content: 'keywords.keywords.keywords.keywords.keywords.keywords.' },
+  ],
+})
 </script>
