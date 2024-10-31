94eb4be67ae8ca4b48a7e7435b09c8ea6cb2ff59
SpiderMan <445001382@qq.com>
2024-10-31 14:51:09 +0800

    feat: 添加eslint-plugin-format支持css,html,markdown格式化

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  // 开启 unocss 支持
+  // 参考 https://github.com/antfu/eslint-config?tab=readme-ov-file#unocss
   unocss: true,
+  // 开启 eslint-plugin-format 格式化css,html,markdown 等文件
+  // 参考 https://github.com/antfu/eslint-config?tab=readme-ov-file#formatters
+  formatters: true,
 })
