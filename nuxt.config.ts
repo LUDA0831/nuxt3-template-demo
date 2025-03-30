103f84746df5931f12fbf9e8ebe616e24bb5fa5b
SpiderMan <445001382@qq.com>
2025-03-30 09:21:31 +0800

    refactor(nuxt): 更新配置文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,26 +12,35 @@ export default defineNuxtConfig({
     'nuxt-swiper',
     'dayjs-nuxt',
   ],
+
   css: ['assets/css/index.css', '@unocss/reset/tailwind-compat.css'],
   runtimeConfig: createRuntimeConfig(),
+
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     keepalive: true,
   },
+
   typescript: {
     shim: false,
     strict: true,
   },
+
   experimental: {
     // 启用新的实验性类型路由器使用 [unplugin-vue-router](https://github.com/posva/unplugin-vue-router).
     typedPages: true,
   },
+
   vite: createViteConfig(),
+
   // vueuse
   vueuse: {
     ssrHandlers: true,
   },
+
   swiper: {
     enableComposables: false,
   },
+
+  compatibilityDate: '2025-03-29',
 })
